fb6bbf70ebe1070011c37a5169be6903c541acd5
= <roulit77@gmail.com>
2021-08-29 23:47:38 +0200

    Correction done

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
-//const helloWorld = React.createElement('h1', {}, 'Hello world!');
-
 const firstName = 'toto';
 const lastName = 'glandu';
 
+function FirstName(props) {
 
-// Solution sans bonus
-const helloWorld = <h1>Hello <span>{firstName[0].toUpperCase() + firstName.substr(1)}</span> <span className="red-text">{lastName.toUpperCase()}</span></h1>;
+    /*
+    // Solution avec bonus
+    const formatFirstName = (firstName) => {
+        return firstName[0].toUpperCase() + firstName.substr(1);
+    }
 
+    return <span>{formatFirstName(props.text)}</span>
+    */
 
-
-/* 
-// Solution avec bonus
-
-function formatFirstName(firstName) {
-    return firstName[0].toUpperCase() + firstName.substr(1);
+    // Solution sans bonus
+    return <span>{props.text}</span>
 }
 
-function formatLastName(lastName){
-    return lastName.toUpperCase();
-    
+function LastName(props) {
+
+    /*
+    // Solution avec bonus
+    const formatLastName = (lastName) => {
+        return lastName.toUpperCase();
+    }
+
+    return <span>{formatLastName(props.text)}</span>
+    */
+
+    // Solution sans bonus
+    return <span className="red-text">{props.text.toUpperCase()}</span>
+
 }
 
-const helloWorld = <h1>Hello <span>{formatFirstName(firstName)}</span> <span className="red-text">{formatLastName(lastName)}</span></h1>;
- */
-
-
+const helloWorld = <h1>Hello <FirstName text={firstName}/> <LastName text={lastName}/></h1>;
 
 ReactDOM.render(helloWorld, document.querySelector('#app'));
