cf7f27b5356c7c99125f621b274479820c6cc4b6
= <roulit77@gmail.com>
2021-08-30 23:07:55 +0200

    Correction done

--- main.js
+++ main.js
@@ -82,18 +82,30 @@ function Clock(props) {
     }, []);
     
     const [date, setDate] = React.useState(new Date());
+    const [textColor, setTextColor] = React.useState("black");
 
     const tick = () => {
         setInterval(() => {
             setDate(new Date())
         }, 1000)
     }
+
+    const changeColor = () => {
+        var randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`; 
+        setTextColor(randomColor);
+    }
+
+    const resetColor = () => {
+        setTextColor('black')
+    }
     
 
     return (
         <div>
             <h1>Hello world</h1>
-            <h2>Il est {date.toLocaleTimeString()}.</h2>
+            <h2 style={{color: textColor}}>Il est {date.toLocaleTimeString()}.</h2>
+            <button onClick={changeColor}>Changer couleur</button>
+            <button onClick={resetColor}>Réinitialiser couleur</button>
         </div>
         );
 }
