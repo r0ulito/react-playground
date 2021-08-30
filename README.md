c715c36849b2147289f32cf4140ab625f6ed8352
= <roulit77@gmail.com>
2021-08-30 08:49:16 +0200

    Correction done

--- README.md
+++ README.md
@@ -1,58 +1,8 @@
-# [État et cycle de vie](https://fr.reactjs.org/docs/state-and-lifecycle.html)
-
-### [Mettre à jour un élément affiché](https://fr.reactjs.org/docs/rendering-elements.html#updating-the-rendered-element)
-
-Avec nos connaissances actuelles, la seule façon de mettre à jour l’interface utilisateur est de créer un nouvel élément et de le passer à `ReactDOM.render()`.
-
-Prenons l'exemple de la documentation:
-
-```javascript
-    function tick() {
-        const element = (
-            <div>
-            <h1>Hello World !</h1>
-            <h2>Il est {new Date().toLocaleTimeString()}.</h2>
-            </div>
-        );
-        ReactDOM.render(element, document.getElementById('root'));
-    }
-    // Chaque seconde, nous appellons ReactDOM.render() depuis un callback passé à setInterval().
-    setInterval(tick, 1000);    
-```
-
-En vous aidant de la documentation modifions ce code pour avoir un composant `Clock`.
-
-```javascript
-    function Clock(props) {
-        return (
-            <div>
-            <h1>Bonjour, monde !</h1>
-            <h2>Il est {props.date.toLocaleTimeString()}.</h2>
-            </div>
-        );
-        }
-
-        function tick() {
-        ReactDOM.render(
-            <Clock date={new Date()} />,
-            document.getElementById('root')
-        );
-    }
-    setInterval(tick, 1000);    
-
-```
-
-## Énoncé
-
-En nous appuyant sur la documentation, modifions le code d'exemple précédent pour en faire un `Class component`.
-
-Puis avec nos connaissances, modifions à nouveau ce code pour en faire un `Functional component`
-
 
 
 ---
 
-Pour passer à la suite vous devez commit vos changements puis checkout sur la branche exo6
+Pour passer à la suite vous devez commit vos changements puis checkout sur la branche exo7
 
 
 
--- main.js
+++ main.js
@@ -1,37 +1,105 @@
-const firstName = 'toto';
-const lastName = 'glandu';
-
-function FirstName(props) {
-
-    /*
-    // Solution avec bonus
-    const formatFirstName = (firstName) => {
-        return firstName[0].toUpperCase() + firstName.substr(1);
+/* 
+// Étape 1
+class Clock extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Bonjour, monde !</h1>
+          <h2>Il est {this.props.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
     }
+  }
 
-    return <span>{formatFirstName(props.text)}</span>
-    */
+ReactDOM.render(<Clock date={new Date()}/>, document.querySelector('#app')); 
 
-    // Solution sans bonus
-    return <span>{props.text[0].toUpperCase() + props.text.substr(1)}</span>
+*/
+
+
+/* 
+
+// Étape 2
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date()};
+    }
+  render() {
+    return (
+      <div>
+        <h1>Bonjour, monde !</h1>
+        <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
-function LastName(props) {
+ReactDOM.render(<Clock />, document.querySelector('#app')); 
 
-    /*
-    // Solution avec bonus
-    const formatLastName = (lastName) => {
-        return lastName.toUpperCase();
+
+ */
+
+
+
+// Étape 3
+
+/* class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date()};
+    }
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(),1000);
     }
 
-    return <span>{formatLastName(props.text)}</span>
-    */
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
 
-    // Solution sans bonus
-    return <span className="red-text">{props.text.toUpperCase()}</span>
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
 
+    render() {
+        return (
+        <div>
+            <h1>Bonjour, monde !</h1>
+            <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+        );
+    }
 }
 
-const helloWorld = <h1>Hello <FirstName text={firstName}/> <LastName text={lastName}/></h1>;
+ReactDOM.render(<Clock />, document.querySelector('#app'));  */
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+
+function Clock(props) {
+    const [date, setDate] = React.useState(new Date());
+
+    const tick = () => {
+        setInterval(() => {
+            setDate(new Date())
+        })
+    }
+    React.useEffect(() => {
+        tick();
+        clearInterval(tick);
+    })
+
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <h2>Il est {date.toLocaleTimeString()}.</h2>
+        </div>
+        );
+}
+
+ReactDOM.render(<Clock />, document.querySelector('#app'));
+
+
+
+
+
