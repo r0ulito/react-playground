2eaa5a2ce740ed001ac4714bb77ae625d029db33
= <roulit77@gmail.com>
2021-12-13 10:22:18 +0100

    changed name to be more student compliant

--- main.js
+++ main.js
@@ -1,29 +1,10 @@
 //const helloWorld = React.createElement('h1', {}, 'Hello world!');
 
-const firstName = 'toto';
-const lastName = 'glandu';
+const firstName = 'r0ulito';
+const lastName = 'formateur';
 
 
 // Solution sans bonus
 const helloWorld = <h1>Hello <span>{firstName[0].toUpperCase() + firstName.substr(1)}</span> <span className="red-text">{lastName.toUpperCase()}</span></h1>;
-
-
-
-/* 
-// Solution avec bonus
-
-function formatFirstName(firstName) {
-    return firstName[0].toUpperCase() + firstName.substr(1);
-}
-
-function formatLastName(lastName){
-    return lastName.toUpperCase();
-    
-}
-
-const helloWorld = <h1>Hello <span>{formatFirstName(firstName)}</span> <span className="red-text">{formatLastName(lastName)}</span></h1>;
- */
-
-
 
 ReactDOM.render(helloWorld, document.querySelector('#app'));
