5cc2d26222e4556df79ffb2709bf4b6860100f04
= <roulit77@gmail.com>
2022-02-07 12:42:37 +0100

    Corrected solution

--- main.js
+++ main.js
@@ -6,6 +6,4 @@ const helloWorld = React.createElement('h1', {}, 'Hello world!');
 
 //ReactDOM.render(helloWorld, document.body);
 
-// Meilleure pratique mais pas encore LA meilleure
-
 ReactDOM.render(helloWorld, document.querySelector('#app'));
