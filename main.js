8572e3fa41b2ad96fa10c111160fd5c244dfc2e3
= <roulit77@gmail.com>
2022-02-07 16:02:00 +0100

    Added bonus solution

--- main.js
+++ main.js
@@ -7,4 +7,26 @@ const lastName = 'formateur';
 // Solution sans bonus
 const helloWorld = <h1>Hello <span>{firstName[0].toUpperCase() + firstName.slice(1)}</span> <span className="red-text">{lastName.toUpperCase()}</span></h1>;
 
+
+
+/**
+ * 
+ * Solution avec bonus
+ */
+
+/*
+ const formatFirstName = (value) => value[0].toUpperCase() + value.slice(1);
+
+ const formatLastName = (value) => value.toUpperCase();
+ 
+ // Solution avec bonus
+ const helloWorld = (
+   <h1>
+     Hello <span>{formatFirstName(firstName)}</span>{" "}
+     <span className="red-text">{formatLastName(lastName)}</span>
+   </h1>
+ );
+
+ */
+
 ReactDOM.render(helloWorld, document.querySelector('#app'));
