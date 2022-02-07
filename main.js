28c82f0ed5592a4bf73f00ca825b1f10d3d5eeb3
MaissamB <maiss.ben@gmail.com>
2022-02-07 16:40:18 +0100

    exo4 over (bonus not donr)

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 //const helloWorld = React.createElement('h1', {}, 'Hello world!');
 
-const firstName = 'r0ulito';
-const lastName = 'formateur';
+const firstName = 'may';
+const lastName = 'Bensaber';
+
+
+class FirstName extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <span>{this.props.name[0].toUpperCase()+this.props.name.slice(1)}</span>;
+    }
+}
+
+
+
+function LastName(props) {
+    return <span className="red-text">{props.name.toUpperCase()}</span>;
+}
+
 
 
 // Solution sans bonus
-const helloWorld = <h1>Hello <span>{firstName[0].toUpperCase() + firstName.slice(1)}</span> <span className="red-text">{lastName.toUpperCase()}</span></h1>;
+const helloWorld = <h1>Hello <FirstName name ={firstName}/> <LastName name={lastName}/></h1>;
 
 ReactDOM.render(helloWorld, document.querySelector('#app'));
+
+
+
+
